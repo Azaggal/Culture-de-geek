dc0cc5bb7d548e3547035d12127525e8d3319eb1
Tim <timote.godard@gmail.com>
2026-02-18 11:16:25 +0100

    ajout de plus de chronologies

--- serveur-quiz/index.js
+++ serveur-quiz/index.js
@@ -597,8 +597,8 @@ function rejoindreLobby({ socket,pseudo, permanentId, roomCode }){
 
 async function nouvelleQuestion(roomCode) {
   const room = rooms[roomCode];
-  //const type = typesDisponibles[Math.floor(Math.random() * typesDisponibles.length)];
-  const type = "wikipedia";
+  const type = typesDisponibles[Math.floor(Math.random() * typesDisponibles.length)];
+  //const type = "wikipedia";
   room.type = type;
   
   let quizData;
